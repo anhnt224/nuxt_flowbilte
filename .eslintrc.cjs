7a4afbb07704ee8a481f4d93bc37d433fb308cce
Nham Trong Anh <anhnt019@gmail.com>
2023-12-20 13:30:18 +0700

    init

---
+++ .eslintrc.cjs
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  root: true,
+  'extends': [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  ignorePatterns: ['tailwind.config.js']
+}
---
+++ nuxt.config.ts
@@ -0,0 +1,16 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import { defineNuxtConfig } from 'nuxt/config'
+
+export default defineNuxtConfig({
+  devtools: { enabled: true }, 
+  modules: [
+    '@nuxtjs/eslint-module'
+  ],
+  css: ['~/assets/css/tailwind.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+})
